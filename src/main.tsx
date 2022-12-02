a64affadd4643576dc6e8bb016e7209fc5dcfd5b
malhaniah <mohd.alhaniah@gmail.com>
2022-12-02 04:36:38 +0300

    loading variables from .env

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { config } from "dotenv";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Loads environment variables from .env file
+config();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
