74015575a5333eda9944f0e9b4fef6cb04bc03c3
malhaniah <mohd.alhaniah@gmail.com>
2022-12-06 13:18:25 +0300

    Changes to be committed:

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export async function getStandings(request: Request, response: Response) {
    const standings = await fetch('https://api.football-data.org/v4/competitions/2000/standings', {
         headers: {
-            'X-Auth-Token': 'e96f4d26094f49e98966f2c9753a29dc'
+            'X-Auth-Token': process.env.API_KEY as string;
         }
         
     })
